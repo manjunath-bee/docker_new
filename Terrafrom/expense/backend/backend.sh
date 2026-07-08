3886b8af88724a38a21b0c45ae7c2acaceca7d4b
Manjunath G <manjudba1222@gamil.com>
2026-07-08 20:01:36 +0530

    install ansible collections

--- Terrafrom/expense/backend/backend.sh
+++ Terrafrom/expense/backend/backend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo dnf install ansible-core -y
+ansible-galaxy collection install community.general community.mysql
 ansible-pull -i localhost, -U https://github.com/manjunath-bee/docker_new.git -d /tmp/ansible-repo anisible-tf/main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
