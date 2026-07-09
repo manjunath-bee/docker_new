d8cd5c6b9adf03fca5f29a474f34548ac90fa3d4
Manjunath G <manjudba1222@gamil.com>
2026-07-09 16:42:14 +0530

    sec

--- Terrafrom/expense/backend/ec2.tf
+++ Terrafrom/expense/backend/ec2.tf
@@ -2,39 +2,17 @@ resource "aws_instance" "example" {
   ami                    = "ami-00b6288e85377e730"
   instance_type          = "t3.large"
   vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg.value]
-  subnet_id = split(",", data.aws_ssm_parameter.private_id.value)[0]
-  key_name = "balu"
+  subnet_id              = split(",", data.aws_ssm_parameter.private_id.value)[0]
+  key_name               = "balu"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo dnf install ansible-core -y
+    ansible-galaxy collection install community.general community.mysql
+    ansible-pull -i localhost, -U https://github.com/manjunath-bee/docker_new.git -d /tmp/ansible-repo anisible-tf/main.yaml -e COMPONENT=backend -e ENVIRONMENT=${var.environment}
+  EOF
+
   tags = {
     Name = "HelloWorld"
-  }
-}
-
-resource "null_resource" "cluster" {
-  # Changes to any instance of the cluster requires re-provisioning
-  triggers = {
-    instance_id = aws_instance.example.id
-  }
-
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
-  
-  connection {
-  host        = aws_instance.example.private_ip
-  type        = "ssh"
-  user        = "ec2-user"
-  private_key = file("C:/Users/manjungj/Downloads/BALU.pem")
-}
-
-  provisioner "file" {
-    source      = "backend.sh"
-    destination = "/tmp/backend.sh"
-  }
-
-  provisioner "remote-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
-    inline = [
-      "chmod +x /tmp/backend.sh",
-      "sudo sh /tmp/backend.sh ${var.environment}"
-    ]
   }
 }
---
+++ Terrafrom/expense/backend/readme.md
@@ -0,0 +1,40 @@
+resource "aws_instance" "example" {
+  ami                    = "ami-00b6288e85377e730"
+  instance_type          = "t3.large"
+  vpc_security_group_ids = [data.aws_ssm_parameter.backend_sg.value]
+  subnet_id = split(",", data.aws_ssm_parameter.private_id.value)[0]
+  key_name = "balu"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers = {
+    instance_id = aws_instance.example.id
+  }
+
+  # Bootstrap script can run on any instance of the cluster
+  # So we just choose the first in this case
+  
+  connection {
+  host        = aws_instance.example.private_ip
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = file("C:/Users/manjungj/Downloads/BALU.pem")
+}
+
+  provisioner "file" {
+    source      = "backend.sh"
+    destination = "/tmp/backend.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "chmod +x /tmp/backend.sh",
+      "sudo sh /tmp/backend.sh ${var.environment}"
+    ]
+  }
+}
